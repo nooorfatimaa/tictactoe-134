4684e2c339aed0b2937cf52e9903164a4bc85ed1
Noor Fatima <noorfatima@noors-macbook.home>
2020-11-27 11:29:53 +0500

    Tic Tac Toe game added

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [boxState, setBoxState] = useState('X');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.grid}>
+        <View style={[styles.lineV , {transform: [{translateX: 132}]}]}/>
+        <View style={[styles.lineV , {transform: [{translateX: 264}]}]}/>
+        <View style={[styles.lineH , {transform: [{translateY: 132}]}]}/>
+        <View style={[styles.lineH , {transform: [{translateY: 264}]}]}/>
+      </View>
     </View>
   );
 }
@@ -14,8 +19,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  grid: {
+    borderColor: 'black',
+    backgroundColor: '#fff',
+    height : 402,
+    width: 402,
+    borderWidth: 3,
+  },
+  lineV: {
+    borderColor: 'black',
+    backgroundColor: 'black',
+    height: 396,
+    width: 1,
+    position: 'absolute',
+  },
+  lineH: {
+    borderColor: 'black',
+    backgroundColor: 'black',
+    height: 1,
+    width: 396,
+    position: 'absolute',
   },
 });
